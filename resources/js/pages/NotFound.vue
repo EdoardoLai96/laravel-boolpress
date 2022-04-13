481c138c5c5891c6c4158fe4facb5d0170a9e341
Edoardo Lai <edoardolai96@gmail.com>
2022-04-13 20:16:25 +0200

    Creazione pagina not found

---
+++ resources/js/pages/NotFound.vue
@@ -0,0 +1,16 @@
+
+<template>
+    <div>
+        <h1 class="text-center">404</h1>
+        <h2 class="text-center">Ops... La pagina che stai cercando non esiste!</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFound'
+}
+</script>
+
+<style>
+</style>
